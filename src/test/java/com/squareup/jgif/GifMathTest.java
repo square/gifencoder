20930eae7bc83a753890872700b251fd4e5de158
Daniel Lubarov <daniel@lubarov.com>
2015-05-04 19:18:56 -0700

    More AssertJ

--- src/test/java/com/squareup/jgif/GifMathTest.java
+++ src/test/java/com/squareup/jgif/GifMathTest.java
@@ -2,14 +2,14 @@ package com.squareup.jgif;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class GifMathTest {
   @Test public void testRoundUpToPowerOfTwo() {
-    assertEquals(1, GifMath.roundUpToPowerOfTwo(1));
-    assertEquals(2, GifMath.roundUpToPowerOfTwo(2));
-    assertEquals(4, GifMath.roundUpToPowerOfTwo(3));
-    assertEquals(4, GifMath.roundUpToPowerOfTwo(4));
-    assertEquals(8, GifMath.roundUpToPowerOfTwo(5));
+    assertThat(GifMath.roundUpToPowerOfTwo(1)).isEqualTo(1);
+    assertThat(GifMath.roundUpToPowerOfTwo(2)).isEqualTo(2);
+    assertThat(GifMath.roundUpToPowerOfTwo(3)).isEqualTo(4);
+    assertThat(GifMath.roundUpToPowerOfTwo(4)).isEqualTo(4);
+    assertThat(GifMath.roundUpToPowerOfTwo(5)).isEqualTo(8);
   }
 }
--- src/test/java/com/squareup/jgif/MedianCutQuantizerTest.java
+++ src/test/java/com/squareup/jgif/MedianCutQuantizerTest.java
@@ -5,12 +5,12 @@ import java.util.HashSet;
 import java.util.Set;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MedianCutQuantizerTest {
   @Test public void testQuantize() {
-    // The red component has the greatest spread, so the original four colors should be split in
-    // half based on their red components.
+    // The red component has the greatest spread, so the original four colors should be divided into
+    // two groups based on their red components.
     Set<Color> originalColors = colors(
         new Color(0, 0, 0),
         new Color(1, 0, 0),
@@ -19,7 +19,8 @@ public class MedianCutQuantizerTest {
     Set<Color> expectedQuantizedColors = colors(
         new Color(0, 0.25, 0),
         new Color(1, 0.25, 0));
-    assertEquals(expectedQuantizedColors, MedianCutQuantizer.INSTANCE.quantize(originalColors, 2));
+    assertThat(MedianCutQuantizer.INSTANCE.quantize(originalColors, 2))
+        .isEqualTo(expectedQuantizedColors);
   }
 
   private static Set<Color> colors(Color... colorArray) {
